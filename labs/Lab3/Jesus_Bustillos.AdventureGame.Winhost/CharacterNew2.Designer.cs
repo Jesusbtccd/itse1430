45463449db7ff92d62d25b5a6021db4de94a7199
Jesus Bustillos <jesus.bustillos@my.tccd.edu>
2022-04-21 20:28:58 -0500

    amended labels

--- labs/Lab3/Jesus_Bustillos.AdventureGame.Winhost/CharacterNew2.Designer.cs
+++ labs/Lab3/Jesus_Bustillos.AdventureGame.Winhost/CharacterNew2.Designer.cs
@@ -39,6 +39,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -54,8 +55,12 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // 
             this.Profession.FormattingEnabled = true;
             this.Profession.Items.AddRange(new object[] {
-            "Profession"});
-            this.Profession.Location = new System.Drawing.Point(22, 12);
+            "Fighter",
+            "Hunter",
+            "Priest",
+            "Rogue",
+            "Wizard"});
+            this.Profession.Location = new System.Drawing.Point(22, 181);
             this.Profession.Name = "Profession";
             this.Profession.Size = new System.Drawing.Size(121, 23);
             this.Profession.TabIndex = 1;
@@ -64,7 +69,13 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(22, 41);
+            this.comboBox2.Items.AddRange(new object[] {
+            "Dwarf",
+            "Elf",
+            "Gnome",
+            "Half Elf",
+            "Human"});
+            this.comboBox2.Location = new System.Drawing.Point(22, 129);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 23);
             this.comboBox2.TabIndex = 2;
@@ -72,7 +83,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 104);
+            this.label2.Location = new System.Drawing.Point(22, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 15);
             this.label2.TabIndex = 3;
@@ -81,7 +92,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 139);
+            this.label3.Location = new System.Drawing.Point(22, 163);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 15);
             this.label3.TabIndex = 4;
@@ -90,7 +101,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 177);
+            this.label4.Location = new System.Drawing.Point(22, 221);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 15);
             this.label4.TabIndex = 5;
@@ -99,7 +110,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 215);
+            this.label5.Location = new System.Drawing.Point(22, 249);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(38, 15);
             this.label5.TabIndex = 6;
@@ -108,7 +119,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 246);
+            this.label6.Location = new System.Drawing.Point(22, 276);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(38, 15);
             this.label6.TabIndex = 7;
@@ -117,7 +128,7 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(22, 279);
+            this.label7.Location = new System.Drawing.Point(22, 359);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(38, 15);
             this.label7.TabIndex = 8;
@@ -126,17 +137,25 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(22, 310);
+            this.label8.Location = new System.Drawing.Point(22, 412);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(38, 15);
             this.label8.TabIndex = 9;
             this.label8.Text = "label8";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 10;
             // 
             // CharacterNew2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -170,5 +189,6 @@ namespace Jesus_Bustillos.AdventureGame.Winhost
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
